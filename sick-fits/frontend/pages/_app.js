249355c02cb0488e069181206361cc42b0b30048
ckkeeley <chelsea.keeley@gmail.com>
2018-11-29 07:59:45 -0500

    Set up apollo client, complete video 15

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,21 +1,36 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 // myapp is going to wrap our app at the highest level with Container from next.js
 // Component that we are taking from props is going to be our pages, ie. index.js, sell.js etc.
 //
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query
+
+    return { pageProps }
+  }
+
   render () {
-    const { Component } = this.props
+    const { Component, apollo, pageProps } = this.props
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps}/>
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
