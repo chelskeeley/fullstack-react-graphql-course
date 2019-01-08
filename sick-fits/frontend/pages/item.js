40ab9e97d73b7d38bf29b923bfccc243b4736cee
ckkeeley <chelsea.keeley@gmail.com>
2019-01-08 07:55:58 -0500

    Display item details page, complete video 21

---
+++ sick-fits/frontend/pages/item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SingleItem from "../components/SingleItem";
+
+const Item = props => (
+  <div>
+    <SingleItem id={props.query.id}/>
+  </div>
+);
+
+
+export default Item;
