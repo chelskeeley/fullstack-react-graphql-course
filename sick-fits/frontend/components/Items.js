5bcdbe33ab904e91d5bd59b1bea98c1a2fb9feec
ckkeeley <chelsea.keeley@gmail.com>
2019-01-26 11:27:56 -0500

    Completed video 23, finished pagination of items page

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -5,10 +5,11 @@ import styled from "styled-components";
 
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -36,7 +37,14 @@ export default class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page}/>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+          skip: this.props.page * perPage - perPage
+          }}
+          // Currently when we add or delete an item, the items page is pulling from the cache instead of refetching the queries, so you cannot see a new item added right away, and deleting an item messes up the pagination. The commented out line below tells the query to hit the network EVERY time we change a page, but this is not ideal as it takes away the current fast/snappy behaviour of using the cache with pagination. No current better solution through apollo, onward ho!
+          // fetchPolicy="network-only"
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
