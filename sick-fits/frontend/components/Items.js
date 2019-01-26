01b2a0265d5181b53eec76dc56587d040ceea120
ckkeeley <chelsea.keeley@gmail.com>
2019-01-26 11:08:11 -0500

    Completed video 22, start of items pagination

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import styled from "styled-components";
 
 import Item from "./Item";
+import Pagination from "./Pagination";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -34,6 +35,7 @@ export default class Items extends Component {
   render() {
     return (
       <Center>
+        <Pagination page={this.props.page}/>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
@@ -43,6 +45,7 @@ export default class Items extends Component {
             </ItemsList>
           }}
         </Query>
+        <Pagination page={this.props.page}/>
       </Center>
     )
   }
