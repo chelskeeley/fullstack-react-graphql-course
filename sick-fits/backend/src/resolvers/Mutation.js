510ff1a907c97bb080fff70645c799f7d11344ac
ckkeeley <chelsea.keeley@gmail.com>
2019-01-28 17:56:04 -0500

    wip video 27 min 9

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -56,9 +56,33 @@ const Mutations = {
     // set the jwt as a cookie on the response
     ctx.response.cookie("token", token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 24 * 365 // 1 year cookie
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
     })
     // fiiinally we return the new user to the browser
+    return user;
+  },
+  async signin(parent, { email, password}, ctx, info) {
+    // 1. Check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+
+    if (!user) {
+      throw new Error(`No user found for email: ${email}`)
+    }
+
+    // 2. Check that the password is correct
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Invalid Password!")
+    }
+    // 3. Generate the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    })
+    // 5. Return the user
     return user;
   }
 };
