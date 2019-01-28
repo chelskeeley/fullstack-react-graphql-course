170799732364c43f30439c31655b37d819550d4c
ckkeeley <chelsea.keeley@gmail.com>
2019-01-28 17:24:33 -0500

    Completed video 26, write User component for current user

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
@@ -7,7 +8,19 @@ const db = require("./db");
 const server = createServer();
 
 server.express.use(cookieParser());
-// TODO use epress middleware to populate current user
+
+// decode the jwt so we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the user Id onto the req for further requests to access
+    req.userId = userId;
+  }
+
+  next();
+})
 
 server.start({
   cors: {
