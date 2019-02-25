02da75f41d9848ba7e17e2816f51035fa20d61ae
ckkeeley <chelsea.keeley@gmail.com>
2019-02-25 13:31:50 -0500

    Completed video 33, gated sign in component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Link from 'next/link';
 
 import CreateItem from "../components/CreateItem";
+import PleaseSignIn from "../components/PleaseSignIn";
 
 const Sell = props => (
   <div>
-    <CreateItem/>
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
 );
 
