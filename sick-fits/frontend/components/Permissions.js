c4fd44d63a8929963b2b4201b8936fc0140fb184
ckkeeley <chelsea.keeley@gmail.com>
2019-03-05 19:50:26 -0500

    Completed video 35, updating permissions in local state

--- sick-fits/frontend/components/Permissions.js
+++ sick-fits/frontend/components/Permissions.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import PropTypes from "prop-types";
 
 import Error from "./ErrorMessage";
 import Table from "./styles/Table";
@@ -44,7 +45,7 @@ const Permissions = props => {
                 </tr>
               </thead>
               <tbody>
-                {data.users.map(user => <User user={user} key={user.email} />)}
+                {data.users.map(user => <UserPermissions user={user} key={user.email} />)}
               </tbody>
             </Table>
           </div>
@@ -55,7 +56,37 @@ const Permissions = props => {
   )
 };
 
-class User extends Component {
+class UserPermissions extends Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }).isRequired
+  }
+
+  // typically we do not want to initialize state with props. In this use case, we are seeding the data, so the initial user data will come from the props saved in state, and any changes will be changed in the LOCAL state, then when we save, the changed data will be sent to the BE
+  state = {
+    permissions: this.props.user.permissions,
+  }
+
+  handlePermissionChange = (e) => {
+    const checkbox = e.target;
+    // take a copy of the current permissions
+    let updatedPermissions = [...this.state.permissions];
+
+    if (checkbox.checked) {
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value);
+    }
+
+    this.setState({
+      permissions: updatedPermissions
+    })
+  }
+
   render() {
     const user = this.props.user;
 
@@ -64,9 +95,14 @@ class User extends Component {
         <td>{user.name}</td>
         <td>{user.email}</td>
         {possiblePermissions.map(permission => (
-          <td>
-            <label htmlFor={`${permission}-permission-${permission}`} key={`${permission}-permission-${permission}`}>
-              <input type="checkbox" />
+          <td key={`${permission}-permission-${permission}`}>
+            <label htmlFor={`${permission}-permission-${permission}`}>
+              <input
+                type="checkbox"
+                checked={this.state.permissions.includes(permission)}
+                value={permission}
+                onChange={this.handlePermissionChange}
+              />
             </label>
           </td>
         ))}
